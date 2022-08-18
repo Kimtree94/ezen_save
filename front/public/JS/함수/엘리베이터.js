f6700ee0b44a6ef39a8dfd2f11b7b6f22ad5f4b4
504 <504@DESKTOP-GBD69DL>
2022-08-18 18:44:22 +0900

    20220818

--- front/public/JS/함수/엘리베이터.js
+++ front/public/JS/함수/엘리베이터.js
@@ -1,57 +1,35 @@
 /*
-let my_floors =[]
-let el_floors =[]
+엘리베이터 버튼 생성 
+버튼 클릭시 실행
+버튼클릭시
+현재 엘리베이터 층수와 내가있는 층 난수발생
+[엘리베이터가 있는 위치 저장할곳][내가 있는층 엘리베이터 위치 저장할곳]
 
+엘리베이터가 내가 있는 층보다 높을시 한층씩 하강됨을 테이블에 표시
+엘리베이터가 내가 있는 층보다 낮을시 한층씩 상승됨을 테이블에 표시
+조건 일치시 결과창에 엘리베이터가 도착했습니다 표시
+html연결
+*/
+//let E_numbers =[]//엘리베이터 층수를 저장할 배열 
+//let M_number =[]//내가 있는층수를 저장할 배열 
+//up버튼을 누를때마다 
+// Math.floor((Math.random()*20+1))
 
-function my_floors(){
-	//난수생성[1~45]
-	while(true) {// 무한루프[ 추첨번호가 6개 되면 종료]
-	 											//	Math.random() 0~1사이의 실수 생성
-	 let index= my_floors.indexOf(rand)
-	 let rand =	Math.floor((Math.random()*10)+1)// (Math.random()*끝값)
-	 if(index == -1){//존재하지 않으면
-		if(lotto_numbers.length==1){//배열내 수가 6개 이면 반복문 종료
-			break//반복문 탈출
-		} end
-		}//if end
-	}//while end
-	//html 태그에 배열 출력
-	document.getElementById('E/lbox').innerHTML=my_floors
-	//결과 함수 출력
-	
-}
-
- let my_floors=[]// 내가 있는 층
- let el_floors=[]//엘리베이터가 있는 층 
- //1. 버튼 만들기 함수
- function btn_print(){
-	let btnlist='' //1개의 버튼을 저장하는 수
-	for(let i=1; i=10;i++){
-		//i는 1부터 10까지 1씩증가 반복
-		btnlist+= `<button id=${i} onclick=btn_click(${i})>${i}</button>`
-		//i 가 5배수이면 줄바꿈
-		if(i%5==0){btnlist+='<br>'}
-	}//for end
-	//현재문서의 btnbox 라는 id 갖는 태그 호출해서 html변수 대입
-	document.getElementById('btnbox').innerHTML = btnlist;
-}
+/*
+const rand1 = Math.floor((Math.random()*21))//엘베층
+const rand2 = Math.floor((Math.random()*21))//내가있는층
 */
 
+const rand1 = 10//엘베층
+const rand2 = 3 //내가있는층
 
+let E_number='' //난수저장
 
-
-
-
-
-
-
-
-
-
-
-
-/*function my_floor(){
-	for(let i=1;i=10;i++){
-		if()
+if(rand1!=rand2){//if1 시작
+	if(rand1>rand2){//2if 시작
+		for(let i= ; i>= ;i++){
+			
+		}
 	}
-}*/
+}
+
