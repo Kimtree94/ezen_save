2211c5e7f3d882acaab26a0c44dca933ae6dd1f8
504 <504@DESKTOP-GBD69DL>
2022-08-30 21:20:17 +0900

    20220830

--- front/public/CSS/웹디자인/chapter4/script.js
+++ front/public/CSS/웹디자인/chapter4/script.js
@@ -1,4 +1,8 @@
 
+
+let 지원자리스트 = [] //지원자 객체들을 담을 배열 선언 : 함수박에 선언하는 이유는 여러함수들에서 사용하기 위해서임
+
+
  /*chartist.js를 이용한 차트 구현*/
 /* 
 	변수 = 값
@@ -88,18 +92,54 @@ new Chartist.Bar('.bar_chart2', barData2, barOptions2);
 	document.getElementById
  */
 
+//과제
+/*
+	0.전송버튼을 눌렀을때 click이벤트 함수 실행
+	
+	1.HTML에서 입력된 데이터 JS 가져오기
+	2.HTML에서 입력된 데이터 JS 가져와서 변수/객ㅌ체 저장 -> 확인
+	  	2-1:문서객체 가져오기
+		document.querySelector( ) : 1개 요소 호출
+		document.querySelectorAll( ) : 1개 요소 호출
+		document.getElementById ( )  : 1개 요소
+		2-2: 문서객체내 내용물 호출[value 속성]
+			*체크박스 여부 확인
+		2-3: 모든 내용물 객체에 저장
+		2-4: 생성된 객체를 배열에 저장
+	3.배열내 존재하는 모든 객체들을 출력
+ */
 
-
-
-
-
-
-
-
-
-
-
-
+//1.
+function 지원자등록(){
+	//1번 테스트
+	console.log('지원자등록 함수 실행')
+	
+	let 입력리스트= document.querySelectorAll('input')
+	let 목록상자= document.querySelector('select')
+	let 긴글상자= document.querySelector('textarea')
+	//2번 테스트
+	console.log(입력리스트)
+	console.log(목록상자)
+	console.log(긴글상자)
+		//2-3: 모든 내용물 객체에 저장
+	let 지원자 = {
+		성명 :입력리스트[0].value ,	//2-2: 문서객체내 내용물 호출[value 속성]
+		영문명 :입력리스트[1].value ,
+		메일주소 :입력리스트[2].value,
+		포트폴리오 :입력리스트[3].value,
+		희망근무지 :입력리스트[4].value,  /*4~6체크박스 */ 
+		현재상태 :목록상자.value,
+		자기소개 :긴글상자.values,
+	}
+	//2-3번 테스트
+	console.log(지원자)
+	//2-4: 생성된 객체를 배열에 저장
+	지원자리스트.push(지원자)
+	console.log(지원자리스트)
+}
+for(let i=0 ;i<지원자리스트.length[i];i++){
+	
+}
 
 
 
