26b22f5877239d76a72694a60120bb67ee28f8f5
504 <504@DESKTOP-GBD69DL>
2022-09-07 20:10:05 +0900

    20220907

--- 연습/public/JS연습/스터디 로또.js
+++ 연습/public/JS연습/스터디 로또.js
@@ -5,7 +5,7 @@ let myn=[]
 
 function creat(){
 	let game=''
-	for(let i=1;i<=46;i++){
+	for(let i=1;i<=45;i++){
      game+=`<button id=${i} onclick=gameboard(${i})>${i}</button>`
 	if(i%5==0){
 	game+=`<br>`
@@ -13,14 +13,4 @@ function creat(){
   }
   document.getElementById('gameboard').innerHTML=game 
   }
- console.log(myn)
-const box=document.querySelector('#money_Number')
-
-function click(i){
-	let click=myn.indexOf(i)
-	if(click>0){
-		alert('이미 선택한 번호입니다.')
-	}
-		
-	
-}
+ 
