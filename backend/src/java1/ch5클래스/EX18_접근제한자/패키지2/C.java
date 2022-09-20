85ac06dc30d547d2b75c70b7c7509d0ddb9ee6ef
JunJungyu <14dbwjddl@naver.com>
2022-09-20 19:33:30 +0900

    20220920

---
+++ backend/src/java1/ch5클래스/EX18_접근제한자/패키지2/C.java
@@ -0,0 +1,9 @@
+package java1.ch5클래스.EX18_접근제한자.패키지2;
+
+//import java1.ch5클래스.EX18_접근제한자.패키지1.A;
+import java1.ch5클래스.EX18_접근제한자.패키지1.B;
+
+public class C {
+//	A a; 다른패키지의 A클래스가 public이 없어 자동적으로default값으로 설정되 호출 부ㅡㄹ가능 
+	B b;
+}
